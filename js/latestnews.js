30d187802183bb2bca80c00385994ac3f2a89c85
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2025-06-16 09:44:27 +0200

    go west son

---
+++ js/latestnews.js
@@ -0,0 +1,73 @@
+const latesto = document.getElementById('latest-stories')
+
+document.addEventListener('DOMContentLoaded', async() => {
+    const API_URL = "http://localhost:5000"
+
+
+    try {
+
+        const response = await fetch(`${API_URL}/public/latest-articles`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if(!response.ok) {
+            console.log("failed to fetch trending news")
+        }
+
+        const latestArticles = await response.json()
+
+        
+
+
+    
+
+        if(latestArticles.latestArticles.length > 0) {
+
+             latestArticles.latestArticles.forEach(article => {
+                const div = document.createElement('div')
+                div.classList.add('trending-item')
+                div.onclick = () => {
+                        
+                        window.location.href = `article.html#${article._id}`;
+                    };
+
+                    div.innerHTML = `
+                    ${article?.title}
+                    `
+
+                                 latesto.appendChild(div)
+
+
+
+
+             })
+
+
+
+        } else {
+
+             const div = document.createElement('div')
+        div.classList.add('trending-item')
+
+        div.innerHTML = `
+        No Trending News Available
+        `
+
+        latesto.appendChild(div)
+
+
+        }
+
+        
+    } catch (error) {
+
+        console.log("Error fetching data:", error)
+
+       
+        
+    }
+
+})
